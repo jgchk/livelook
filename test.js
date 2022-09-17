344d6dc38124330e33417a57a15cc2e0717b2e56
Jake Cheek <jake@f-m.fm>
2022-09-16 22:46:12 -0400

    prettier

--- test.js
+++ test.js
@@ -1,39 +1,39 @@
-const LiveLook = require('./');
+const LiveLook = require('./')
 
 let livelook = new LiveLook({
-    username: 'toadtripler',
-    password: 'DEFINITELY TRIPLES TOADS',
-    sharedFolder: './mp3s',
-    description: 'not responsible for toad-related mutations',
-    autojoin: [ 'nicotine' ]
-});
+  username: 'toadtripler',
+  password: 'DEFINITELY TRIPLES TOADS',
+  sharedFolder: './mp3s',
+  description: 'not responsible for toad-related mutations',
+  autojoin: ['nicotine'],
+})
 
-livelook.on('error', err => console.error(err));
+livelook.on('error', (err) => console.error(err))
 
 livelook.login((err, res) => {
-    if (err) {
-        return console.error(err);
-    } else if (!res.success) {
-        return console.log('invalid password lol');
+  if (err) {
+    return console.error(err)
+  } else if (!res.success) {
+    return console.log('invalid password lol')
+  }
+
+  livelook.on('sayChatroom', (msg) => {
+    if (msg.room) {
+      console.log(`[${msg.room}] <${msg.username}> ${msg.message}`)
+    } else {
+      console.log(`<${msg.username}> ${msg.message}`)
     }
+  })
 
-    livelook.on('sayChatroom', msg => {
-        if (msg.room) {
-            console.log(`[${msg.room}] <${msg.username}> ${msg.message}`);
-        } else {
-            console.log(`<${msg.username}> ${msg.message}`);
-        }
-    });
+  livelook.on('messageUser', (msg) => {
+    console.log(`<${msg.username}> ${msg.message}`)
+    livelook.messageUser(msg.username, 'hey fug you guy xD')
+  })
 
-    livelook.on('messageUser', msg => {
-        console.log(`<${msg.username}> ${msg.message}`);
-        livelook.messageUser(msg.username, 'hey fug you guy xD');
-    });
-
-    setTimeout(() => {
-        //livelook.getPeerAddress('frogdoubler', console.log);
-        livelook.connectToPeerUsername('fourfish', (err, peer) => {
-            console.log(err, peer);
-        });
-    }, 1000);
-});
+  setTimeout(() => {
+    //livelook.getPeerAddress('frogdoubler', console.log);
+    livelook.connectToPeerUsername('fourfish', (err, peer) => {
+      console.log(err, peer)
+    })
+  }, 1000)
+})
