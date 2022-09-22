cac470ad8b10b30ca27a063a0c7db8df2219d3d7
Jake Cheek <jake@f-m.fm>
2022-09-22 18:55:47 -0400

    update from-distrib to ts

--- lib/message/from-distrib.ts
+++ lib/message/from-distrib.ts
@@ -1,29 +1,36 @@
-export default {
-  0: (msg) => {
-    let decoded = { type: 'ping' }
+import MessageReader from './reader'
 
-    if (msg.length >= 4) {
+export type Ping = {
+  type: 'ping'
+  something?: number
+}
+
+export default {
+  0: (msg: MessageReader) => {
+    const decoded: Ping = { type: 'ping' }
+
+    if (msg.size() >= 4) {
       decoded.something = msg.int32()
     }
 
     return decoded
   },
-  3: (msg) => ({
+  3: (msg: MessageReader) => ({
     type: 'search',
     something: msg.int32(),
     username: msg.str(),
     token: msg.int32(),
     query: msg.str(),
   }),
-  4: (msg) => ({
+  4: (msg: MessageReader) => ({
     type: 'branchLevel',
     level: msg.int32(),
   }),
-  5: (msg) => ({
+  5: (msg: MessageReader) => ({
     type: 'branchRoot',
     root: msg.str(),
   }),
-  7: (msg) => ({
+  7: (msg: MessageReader) => ({
     type: 'childDepth',
     depth: msg.int32(),
   }),
